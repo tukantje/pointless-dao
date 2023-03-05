5d65a5bdf21b85e93fb42e2fb8c7cffb0de73da3
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-03-05 19:31:04 +0100

    Add SDK initializer script

---
+++ scripts/initialize-sdk.ts
@@ -0,0 +1,40 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY === "") {
+  console.error("🔴 Private key not found.");
+  process.exit(1);
+}
+
+if (!process.env.QUICKNODE_API_URL || process.env.QUICKNODE_API_URL === "") {
+  console.error("🔴 QuickNode API URL not found.");
+  process.exit(1);
+}
+
+if (!process.env.WALLET_ADDRESS || process.env.WALLET_ADDRESS === "") {
+  console.error("🔴 Wallet address not found.");
+  process.exit(1);
+}
+
+const sdk = ThirdwebSDK.fromPrivateKey(
+  process.env.PRIVATE_KEY,
+  process.env.QUICKNODE_API_URL
+);
+
+(async () => {
+  try {
+    const address = await sdk.getSigner()?.getAddress();
+
+    if (address) {
+      console.log("👋 SDK initialized by address: ", address);
+    } else {
+      throw new Error("Could not initialize: No address received.");
+    }
+  } catch (err) {
+    console.error("🔴 Failed to get apps from the sdk: ", err);
+    process.exit(1);
+  }
+})();
+
+export default sdk;
